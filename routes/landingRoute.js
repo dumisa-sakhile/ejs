35db6ab5762b60c0b0e2829ba1b7a91ca9b1b95b
Sakhile Dumisa <dumisa.developer@gmail.com>
2024-04-11 20:57:14 +0200

    Update landingRoute.js
    Fix controller bug

--- routes/landingRoute.js
+++ routes/landingRoute.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const {
   aboutController,
   indexRouter,
-} = require('controllers/controller');
+} = require('./controllers/controller');
 
 router.get("/", indexRouter);
 
